c2b7f7cf9b2ec5cd2e6db8c3b52d04c3d9389552
acastillo456 <acastillo@develoop.net>
2022-05-05 16:12:58 -0400

    Impreso paquete horas Equipyacht no recupera totales

--- custom_modules/hr_timesheet_custom/__manifest__.py
+++ custom_modules/hr_timesheet_custom/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['hr_timesheet'],
+    'depends': ['hr_timesheet','analytic'],
 
     # always loaded
     'data': [

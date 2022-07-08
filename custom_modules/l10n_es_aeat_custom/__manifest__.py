f13a88ae9f2ad5b6ab58b3f0065a664ab1a07a38
acastillo456 <acastillo@develoop.net>
2022-07-08 10:17:07 -0400

    l10n_es_aeat

---
+++ custom_modules/l10n_es_aeat_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'l10n_es_aeat Custom',
+    'version': '13.0.0.0',
+    'description': """
+        Modulo que personaliza modulos l10n_es_aeat
+    """,
+    'author': 'Develoop Software S.A.',
+    'category': 'Recupalet',
+    'website': 'https://www.develoop.net/',
+    'depends': ['base', 'account', 'l10n_es_aeat'],
+    'summary': 'Modulo que personaliza modulos l10n_es_aeat',
+    'data': [
+        'views/account_account.xml',
+    ],
+    'qweb': [],
+    'images': ['static/description/icon.png'],
+    'demo': [],
+    'installable': True,
+}
---
+++ custom_modules/l10n_es_aeat_custom/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import account_account
+from . import l10n_es_aeat_report_tax_mapping

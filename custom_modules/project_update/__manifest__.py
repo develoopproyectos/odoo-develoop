cfc941efc82715fa9d7c0bcae9f9607bcc5b466d
develoopproyectos <88203730+develoopproyectos@users.noreply.github.com>
2022-07-21 17:41:38 +0200

    modificar manifest

--- custom_modules/project_update/__manifest__.py
+++ custom_modules/project_update/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "Marc Cortadellas",
-    'website': "http://www.yourcompany.com",
+    'website': "http://develoop.net",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml

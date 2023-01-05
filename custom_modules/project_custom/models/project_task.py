5b6de7742d267053ba5e9bc9f0b4009b104e3ce3
Andres <acastillo@develoop.net>
2023-01-05 15:51:44 -0400

    Update

--- custom_modules/project_custom/models/project_task.py
+++ custom_modules/project_custom/models/project_task.py
@@ -13,7 +13,7 @@ class Dev_ProjectTaskCustom(models.Model):
             if vals.get('planned_hours') == 0:
                 raise ValidationError("El campo horas planeadas es obligatorio")
         else:
-            if self.planned_hours == 0 and not vals.get('project_id',False):
+            if self.planned_hours == 0 and not vals.get('project_id',False) and not vals.get('stage_id',False):
                 raise ValidationError("El campo horas planeadas es obligatorio")
 
         result = super(Dev_ProjectTaskCustom, self).write(vals)

328bf77760c2deb40723522e079526d07d9832ed
Andres <acastillo@develoop.net>
2023-01-05 10:49:54 -0400

    - TAREAS -> Horas planeadas que sea mandatorio - PLANIFICACION -> Ver la tarea en el mouse over

---
+++ custom_modules/project_custom/models/project_task.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+from odoo.exceptions import ValidationError
+
+class Dev_ProjectTaskCustom(models.Model):
+    
+    _inherit = "project.task"
+
+    def write(self, vals):
+        #if vals.get('planned_hours',False):
+        if "planned_hours" in vals:
+            if vals.get('planned_hours') == 0:
+                raise ValidationError("El campo horas planeadas es obligatorio")
+        else:
+            if self.planned_hours == 0:
+                raise ValidationError("El campo horas planeadas es obligatorio")
+
+        result = super(Dev_ProjectTaskCustom, self).write(vals)
+        return result

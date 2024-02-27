3a1e7b329dde37860bbcaec90f137aa7f35a821e
alexFlorido <alexflorido0897@gmail.com>
2024-02-27 14:56:37 -0400

    [REFACTOR]Cambio de color con Fecha planeada y agregado de motivo de cambio de fecha

--- custom_modules/planning_slot_custom/models/planning_slot.py
+++ custom_modules/planning_slot_custom/models/planning_slot.py
@@ -10,6 +10,7 @@ class dev_planning_slot_custom(models.Model):
     
     _inherit = "planning.slot"
 
+    planned_date_end = fields.Datetime(related='task_id.planned_date_end')
     x_expiration_date = fields.Date(related='task_id.date_deadline')
     x_kanban_state = fields.Selection(related='task_id.kanban_state')
     x_stage_id =  fields.Many2one(related='task_id.stage_id')

38f6cb62cb16fcd6d533473581598900053e2a1a
bill-develoop <bhansel@develoop.net>
2024-08-20 17:32:38 -0400

    [FEAT] taskid-25282 -> Añadir indicador límite de horas en planificación

---
+++ custom_modules/hr_employee_custom/models/hr_employee_inherit.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+
+class EmployeeInherit(models.Model):
+    _inherit = 'hr.employee'
+
+    x_plannable_hours = fields.Integer(string="Horas planificables")
+
+
+    @api.depends('x_plannable_hours')
+    def check_x_plannable_hours(self):
+        for rec in self:
+            if rec.x_plannable_hours < 0 and rec.x_plannable_hours > 12:
+                raise ValidationError(_("Las horas planificables no pueden ser menos de 0 o mas de 12 "))
+        return True

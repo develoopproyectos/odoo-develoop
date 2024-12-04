bc8b5c05098cc0d19d4a53336dc63a27581eb524
alexFlorido <alexflorido0897@gmail.com>
2024-12-04 16:08:52 -0400

    TEST

--- custom_modules/project_update/models/project_task.py
+++ custom_modules/project_update/models/project_task.py
@@ -15,7 +15,7 @@ class TaskTemplate(models.Model):
     visible = fields.Boolean(default=False)
     first_attempt =fields.Boolean(default=False)
     motive_deadline = fields.Selection([('t1', 'Reestructuración proyecto'), ('t2', 'Falta de recursos'), ('t3', 'Replanificación otros proyectos'), ('t4','Falta material'), ('t5', 'falta de comunicación'), ('t6', 'Feedback')],
-                                       string="Motivo", tracking = True, invisible=True)
+                                       string="Motivo", tracking = True)
 
 
     @api.onchange('date_deadline')

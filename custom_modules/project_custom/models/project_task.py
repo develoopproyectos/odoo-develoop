b4155093060ced2813da30779fb8950376411056
alexFlorido <alexflorido0897@gmail.com>
2024-12-04 09:42:45 -0400

    [FEAT] Correcciones en modulo de tareas

--- custom_modules/project_custom/models/project_task.py
+++ custom_modules/project_custom/models/project_task.py
@@ -35,11 +35,12 @@ class Dev_ProjectTaskCustom(models.Model):
 
     def get_x_planning_slot(self):
         for rec in self:
-            plannings = self.env['planning.slot'].search([('task_id','=',rec.id)])
-            rec.x_planning_slot = plannings
+            # plannings = self.env['planning.slot'].search([('task_id','=',rec.id)])
+            # rec.x_planning_slot = plannings
+            rec.x_planning_slot = False
             rec.x_planning_slot_str = ""
-            for data in plannings:
-                rec.x_planning_slot_str += "<span>{} - {} ({}) - {} </span><br/>".format(data.start_datetime.strftime("%m/%d/%Y"), data.employee_id.name, str(data.allocated_hours), data.end_datetime.strftime("%m/%d/%Y"))
+            # for data in plannings:
+            #     rec.x_planning_slot_str += "<span>{} - {} ({}) - {} </span><br/>".format(data.start_datetime.strftime("%m/%d/%Y"), data.employee_id.name, str(data.allocated_hours), data.end_datetime.strftime("%m/%d/%Y"))
 
     def get_x_is_planning_delay(self):
         for rec in self:
@@ -90,19 +91,19 @@ class Dev_ProjectTaskCustom(models.Model):
                 stage_name = rec.stage_id.name.lower()
                 if vals.get('stage_id', False):
                     stage_name = self.env['project.task.type'].browse(vals.get('stage_id')).name
-                if stage_name in task_type_validation:
-                    if vals.get('planned_hours', rec.planned_hours) == 0 and (\
-                            vals.get('name', False) or 
-                            vals.get('project_id', False) or 
-                            vals.get('sprint', False) or 
-                            vals.get('user_id', False) or
-                            vals.get('sequence', False) or
-                            vals.get('date_deadline', False) or
-                            vals.get('tag_ids', False) or
-                            vals.get('planned_hours', False) or
-                            vals.get('description', False)
-                            ):
-                        raise ValidationError("El campo horas planeadas es obligatorio")
+                # if stage_name in task_type_validation:
+                #     if vals.get('planned_hours', rec.planned_hours) == 0 and (\
+                #             vals.get('name', False) or 
+                #             vals.get('project_id', False) or 
+                #             vals.get('sprint', False) or 
+                #             vals.get('user_id', False) or
+                #             vals.get('sequence', False) or
+                #             vals.get('date_deadline', False) or
+                #             vals.get('tag_ids', False) or
+                #             vals.get('planned_hours', False) or
+                #             vals.get('description', False)
+                #             ):
+                #         raise ValidationError("El campo horas planeadas es obligatorio")
                     
             if 'stage_id' in vals:
                 stage = self.env['project.task.type'].browse(vals.get('stage_id')).name

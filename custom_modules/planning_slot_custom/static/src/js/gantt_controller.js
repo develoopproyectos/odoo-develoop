95b53682ade287ff3edea136ce95de3922ac42da
rllusco <rllusco@develoop.net>
2025-01-27 11:30:33 -0400

    [FEAT] validate model

--- custom_modules/planning_slot_custom/static/src/js/gantt_controller.js
+++ custom_modules/planning_slot_custom/static/src/js/gantt_controller.js
@@ -5,10 +5,13 @@ import { GanttController } from '@web_gantt/gantt_controller';
 
 patch(GanttController.prototype, {
   async setup() {   
+    debugger
     // TODO =========== CAMBIO HERENCIA - NEW =============
-    this.props.modelParams.metaData.decorationFields.push('x_stage_id');
-    this.props.modelParams.metaData.decorationFields.push('x_task_date_deadline'); 
-    this.props.modelParams.metaData.decorationFields.push('x_resourse_plannable_hours'); 
+    if(this.env.searchModel.resModel === 'planning.slot'){
+      this.props.modelParams.metaData.decorationFields.push('x_stage_id');
+      this.props.modelParams.metaData.decorationFields.push('x_task_date_deadline'); 
+      this.props.modelParams.metaData.decorationFields.push('x_resourse_plannable_hours'); 
+    }
     // TODO =========== END    =============
     super.setup(); // By using the super function, we can execute all parent functions along with ours.
   },

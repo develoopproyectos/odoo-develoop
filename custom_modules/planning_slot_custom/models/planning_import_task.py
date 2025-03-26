445f885ca87db798da6081031dee2950fbb0e642
bill-develoop <bhansel@develoop.net>
2025-03-26 09:56:29 -0400

    [FEAT] Import Data for Tasks And Planning

--- custom_modules/planning_slot_custom/models/planning_import_task.py
+++ custom_modules/planning_slot_custom/models/planning_import_task.py
@@ -24,10 +24,12 @@ class PlanningImportTask(models.TransientModel):
                 if not planning:
                     print(f"No se encontro con planning {planning_id}")
                     continue                
-                init_hours = planning.allocated_hours
-                planning.write({'allocated_hours':False, 'allocated_percentage': False})
-                planning.write({'allocated_hours':init_hours})
-                #planning._compute_allocated_percentage()
+                #init_hours = planning.allocated_hours
+                #planning.write({'allocated_hours':False, 'allocated_percentage': False})
+                #planning.write({'allocated_hours':init_hours})
+                if planning.start_datetime.year == 2025:
+                    planning._compute_allocated_percentage()
+                    _logger.info('Planning Updated: %s', planning.start_datetime.strftime('%d-%m-%Y %H:%M'))
                 if not task_id:
                     continue 
                 task = self.env['project.task'].search([('id', '=', task_id)], limit = 1)

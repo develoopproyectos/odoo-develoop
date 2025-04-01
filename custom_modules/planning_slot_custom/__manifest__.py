92589953141eff28616f6ed7b5e744da1a00814f
rllusco <rllusco@develoop.net>
2025-04-01 08:54:44 -0400

    [FIX:31556] hide btn planning  Automatically plan

--- custom_modules/planning_slot_custom/__manifest__.py
+++ custom_modules/planning_slot_custom/__manifest__.py
@@ -18,10 +18,9 @@
     'assets': {
         'web.assets_backend': [
             'planning_slot_custom/static/src/css/style.css',
-            #'planning_slot_custom/static/src/js/gantt_row.js',            
-			#'planning_slot_custom/static/src/js/progress_bar.js',
             'planning_slot_custom/static/src/js/gantt_renderer.js',
             'planning_slot_custom/static/src/js/gantt_controller.js',
+            'planning_slot_custom/static/src/xml/planning_gantt.xml'
         ],
     },
     'license': 'LGPL-3',

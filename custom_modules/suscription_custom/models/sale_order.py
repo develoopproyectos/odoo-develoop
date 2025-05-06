59309c31d0c6b81cfa3241bfa95272d58cb0e791
Bill Crespo <bhansel@develoop.net>
2025-05-06 09:31:18 -0400

    [FIX] taskid-31580 -> Quitar personalización no usada y agregar dependencias

--- custom_modules/suscription_custom/models/sale_order.py
+++ custom_modules/suscription_custom/models/sale_order.py
@@ -1,8 +1,10 @@
 
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api
-from odoo.osv import expression
+from odoo import models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
 
@@ -12,5 +14,5 @@ class SaleOrder(models.Model):
         """ Hook for extension, to support different invoice states """
         ###EVITAR QUE LAS NUEVAS FACTURAS CREADAS POR LA SUBSCRIPCION SE PUBLIQUEN O CONFIRMEN DE MANERA AUTOMATICA
         #invoice.action_post()
-        print(invoice)
+        _logger.info(f"Factura: {invoice}")      
         return

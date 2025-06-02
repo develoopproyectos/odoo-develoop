93276fe23c4e58b5fdf4d9867bacf866eb944931
rllusco <rllusco@develoop.net>
2025-06-02 11:49:16 -0400

    [FIX] erro validate

--- custom_modules/project_custom/models/project_task.py
+++ custom_modules/project_custom/models/project_task.py
@@ -72,19 +72,19 @@ class Dev_ProjectTaskCustom(models.Model):
                 if vals.get('stage_id', False):
                     stage_name = self.env['project.task.type'].browse(vals.get('stage_id')).name
                 
-                if stage_name in task_type_validation:
-                    if vals.get('allocated_hours', rec.allocated_hours) == 0 and (\
-                            vals.get('name', False) or 
-                            vals.get('project_id', False) or 
-                            vals.get('sprint', False) or 
-                            vals.get('user_id', False) or
-                            vals.get('sequence', False) or
-                            vals.get('date_deadline', False) or
-                            vals.get('tag_ids', False) or
-                            vals.get('allocated_hours', False) or
-                            vals.get('description', False)
-                            ):
-                        raise ValidationError("El campo horas planeadas es obligatorio")
+                # if stage_name in task_type_validation:
+                #     if vals.get('allocated_hours', rec.allocated_hours) == 0 and (\
+                #             vals.get('name', False) or 
+                #             vals.get('project_id', False) or 
+                #             vals.get('sprint', False) or 
+                #             vals.get('user_id', False) or
+                #             vals.get('sequence', False) or
+                #             vals.get('date_deadline', False) or
+                #             vals.get('tag_ids', False) or
+                #             vals.get('allocated_hours', False) or
+                #             vals.get('description', False)
+                #             ):  
+                #         raise ValidationError("El campo horas planeadas es obligatorio")
                     
             if 'stage_id' in vals:
                 stage = self.env['project.task.type'].browse(vals.get('stage_id')).name
